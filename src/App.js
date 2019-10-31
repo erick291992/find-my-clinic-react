d3cec29e32aef5f29dea3a49d4333a6127bfe101
Erick Manrique <erickmanrique@Ericks-MBP.fios-router.home>
2019-10-30 23:37:25 -0400

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import Navbar from './component/Navbar'
 import Home from './container/Home'
 import About from './container/About'
@@ -10,134 +10,149 @@ import FilterResults from './container/FilterResults'
 import Details from './container/Details'
 import Notfound from './container/Notfound'
 import clsx from "clsx";
-import { makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Mydrawer from './component/Mydrawer'
 import Termsofservice from './container/Termsofservice';
 import PrivacyPolicy from './container/PrivacyPolicy';
 import Footer from "./component/Footer";
+import ReactGA from 'react-ga';
+
+const history = createBrowserHistory()
+
+const trackPageView = location => {
+	ReactGA.set({ page: location.pathname });
+	ReactGA.pageview(location.pathname);
+};
+
+function initializeReactGA(history) {
+	ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+	trackPageView(history.location);
+	history.listen(trackPageView);
+}
+
 const drawerWidth = 240;
 
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    height:"100%",
-    fontFamily:"Arial"
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginRight: drawerWidth
-  },
-  title: {
-    flexGrow: 1
-  },
-  hide: {
-    display: "none"
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: drawerWidth
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-start"
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(0),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginRight: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginRight: 0
-  }
+	root: {
+		display: "flex",
+		height: "100%",
+		fontFamily: "Arial"
+	},
+	appBar: {
+		transition: theme.transitions.create(["margin", "width"], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		})
+	},
+	appBarShift: {
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(["margin", "width"], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		}),
+		marginRight: drawerWidth
+	},
+	title: {
+		flexGrow: 1
+	},
+	hide: {
+		display: "none"
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0
+	},
+	drawerPaper: {
+		width: drawerWidth
+	},
+	drawerHeader: {
+		display: "flex",
+		alignItems: "center",
+		padding: theme.spacing(0, 1),
+		...theme.mixins.toolbar,
+		justifyContent: "flex-start"
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(0),
+		transition: theme.transitions.create("margin", {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		}),
+		marginRight: -drawerWidth
+	},
+	contentShift: {
+		transition: theme.transitions.create("margin", {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		}),
+		marginRight: 0
+	}
 }));
 
 
 function App() {
+	initializeReactGA(history);
+	const classes = useStyles();
+	const [open, setOpen] = React.useState(false);
+	let width = window.innerWidth
+	let styleMain = width < 600 ? MainStyleMovil : MainStyleDesktop
+	function handleDrawerOpen() {
+		setOpen(true);
+	}
 
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  let width = window.innerWidth
-  let styleMain = width<600?MainStyleMovil:MainStyleDesktop
-  function handleDrawerOpen() {
-    setOpen(true);
-  }
+	function handleDrawerClose() {
+		setOpen(false);
+	}
 
-  function handleDrawerClose() {
-    setOpen(false);
-  }
+	return (
+		<Router history={history}>
+			<div className={classes.root}>
 
-  return (
-    <Router history={createBrowserHistory()}>
-    <div className={classes.root}>
-
-      <CssBaseline />
-      <Navbar handleDrawerOpen={handleDrawerOpen} open={open} />
-      <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: open
-        })}
-        style={styleMain}
-      >
-        <div className={classes.drawerHeader} />
-          <div >
-            <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route path="/home" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/lawyer" component={Lawyer} />
-              <Route path="/clinic-details" component={Details} />
-              <Route path="/results" component={FilterResults} />
-              <Route path="/clinics" component={ResultsDetails} />
-              <Route path="/terms-of-service" component={Termsofservice} />
-              <Route path="/privacy-policy" component={PrivacyPolicy} />
-              <Route component={Notfound} />
-            </Switch>
-          </div>
-      </main>
-      <Mydrawer 
-          open={open}
-          handleDrawerClose={handleDrawerClose}
-          drawer={classes.drawer} 
-          drawerPaper={classes.drawerPaper}
-          drawerHeader={classes.drawerHeader}/>
-    </div>
-    <Footer />
-    </Router>
-  );
+				<CssBaseline />
+				<Navbar handleDrawerOpen={handleDrawerOpen} open={open} />
+				<main
+					className={clsx(classes.content, {
+						[classes.contentShift]: open
+					})}
+					style={styleMain}
+				>
+					<div className={classes.drawerHeader} />
+					<div >
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route path="/home" component={Home} />
+							<Route path="/about" component={About} />
+							<Route path="/lawyer" component={Lawyer} />
+							<Route path="/clinic-details" component={Details} />
+							<Route path="/results" component={FilterResults} />
+							<Route path="/clinics" component={ResultsDetails} />
+							<Route path="/terms-of-service" component={Termsofservice} />
+							<Route path="/privacy-policy" component={PrivacyPolicy} />
+							<Route component={Notfound} />
+						</Switch>
+					</div>
+				</main>
+				<Mydrawer
+					open={open}
+					handleDrawerClose={handleDrawerClose}
+					drawer={classes.drawer}
+					drawerPaper={classes.drawerPaper}
+					drawerHeader={classes.drawerHeader} />
+			</div>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
 
-const MainStyleDesktop ={
-  paddingTop:"58px"
+const MainStyleDesktop = {
+	paddingTop: "58px"
 }
 
-const MainStyleMovil ={
-  paddingTop:"120px"
+const MainStyleMovil = {
+	paddingTop: "120px"
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,110 +1,110 @@
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 
 export let changeTo = (path) => {
-    const history = createHistory();
-    history.push('/'+path);
-    history.go('/')
+	const history = createBrowserHistory();
+	history.push('/' + path);
+	history.go('/')
 }
 
-export let removeCategory = (list, category) =>{
-    return list.filter(function(ele){
-        return ele !== category;
-    });
+export let removeCategory = (list, category) => {
+	return list.filter(function(ele) {
+		return ele !== category;
+	});
 }
 
 // save filters(category)
 export let saveFilters = (filters) => {
-    localStorage.setItem("filters",JSON.stringify(filters))
+	localStorage.setItem("filters", JSON.stringify(filters))
 }
 
 export let getFilters = () => {
-    return JSON.parse(localStorage.getItem("filters"))
+	return JSON.parse(localStorage.getItem("filters"))
 }
 //save zipcode
 export let saveZipcode = (zipcode) => {
-    localStorage.setItem("zipcode",zipcode)
+	localStorage.setItem("zipcode", zipcode)
 }
 
 export let getZipcode = () => {
-    return localStorage.getItem("zipcode")
+	return localStorage.getItem("zipcode")
 }
 
 export let cleanFilterStorage = () => {
-    //localStorage.clear()
-    localStorage.removeItem('zipcode');
-    localStorage.removeItem('filters');
-    localStorage.removeItem('selectedClinic');
+	//localStorage.clear()
+	localStorage.removeItem('zipcode');
+	localStorage.removeItem('filters');
+	localStorage.removeItem('selectedClinic');
 }
 
 //Save Selected Clinic from map
 export let saveSelectedClinic = (clinic) => {
-    localStorage.setItem("selectedClinic",JSON.stringify(clinic))
+	localStorage.setItem("selectedClinic", JSON.stringify(clinic))
 }
 
 export let getSelectedClinic = () => {
-    return JSON.parse(localStorage.getItem("selectedClinic"))
+	return JSON.parse(localStorage.getItem("selectedClinic"))
 }
 
-export let reOrderList = (clinic,list) =>{
+export let reOrderList = (clinic, list) => {
 
-        let newList = [];
-        newList.push(clinic);
-        list.forEach(lis => {
-          if (clinic._id !== lis._id) {
-            newList.push(lis);
-          }
-        });
-        return newList;
+	let newList = [];
+	newList.push(clinic);
+	list.forEach(lis => {
+		if (clinic._id !== lis._id) {
+			newList.push(lis);
+		}
+	});
+	return newList;
 }
 
-export let newOrderList = (id,list) => {
-    let indexNumber=-1
-    let selected = null
-    let num =null 
-    list.forEach(item=>{
-    indexNumber=indexNumber+1
-    if(item.id===id){
-        num = indexNumber
-        selected= item
-        return
-    }
-    })
+export let newOrderList = (id, list) => {
+	let indexNumber = -1
+	let selected = null
+	let num = null
+	list.forEach(item => {
+		indexNumber = indexNumber + 1
+		if (item.id === id) {
+			num = indexNumber
+			selected = item
+			return
+		}
+	})
 
-    delete list[num]
-    list.splice(num,1)
-    list.unshift(selected)
-    return list
+	delete list[num]
+	list.splice(num, 1)
+	list.unshift(selected)
+	return list
 }
 
-export let findIconPath = (categoryName) =>{
-    let pathImage = ""
-    switch(categoryName){
-        case "Housing": pathImage = "./icons/housing-icon.png"
-            break;
-        case "Consumer": pathImage = "./icons/consumer-icon.png"
-            break;
-         case "Family": pathImage = "./icons/family-icon.png"
-            break;
-        case "Criminal": pathImage = "./icons/criminal-icon.png"
-            break;
-        case "Business": pathImage = "./icons/business-icon.png"
-            break;
-        case "Real Estate": pathImage = "./icons/realstate-icon.png"
-            break;
-        case "General": pathImage = "./icons/general-icon.png"
-            break;
-        case "Trusts & Estates": pathImage = "./icons/trustandstates-icon.png"
-            break;
-        case "Medical": pathImage = "./icons/medical-icon.png"
-            break;
-        case "Legal Procedure": pathImage = "./icons/legalprocedure-icon.png"
-            break;
-        case "Employment": pathImage = "./icons/employment-icon.png"
-            break;
-        case "Immigration": pathImage = "./icons/immigration-icon.png"
-            break;
-        default:
-            console.log("No path...")
-    }
-    return pathImage;
+export let findIconPath = (categoryName) => {
+	let pathImage = ""
+	switch (categoryName) {
+		case "Housing": pathImage = "./icons/housing-icon.png"
+			break;
+		case "Consumer": pathImage = "./icons/consumer-icon.png"
+			break;
+		case "Family": pathImage = "./icons/family-icon.png"
+			break;
+		case "Criminal": pathImage = "./icons/criminal-icon.png"
+			break;
+		case "Business": pathImage = "./icons/business-icon.png"
+			break;
+		case "Real Estate": pathImage = "./icons/realstate-icon.png"
+			break;
+		case "General": pathImage = "./icons/general-icon.png"
+			break;
+		case "Trusts & Estates": pathImage = "./icons/trustandstates-icon.png"
+			break;
+		case "Medical": pathImage = "./icons/medical-icon.png"
+			break;
+		case "Legal Procedure": pathImage = "./icons/legalprocedure-icon.png"
+			break;
+		case "Employment": pathImage = "./icons/employment-icon.png"
+			break;
+		case "Immigration": pathImage = "./icons/immigration-icon.png"
+			break;
+		default:
+			console.log("No path...")
+	}
+	return pathImage;
 }
